0af4b108b20cab5510473e1c8625c9b6613e35d4
lataba <ltballeste@gmail.com>
2023-05-16 11:31:10 -0700

    adding an auxiliar function to measures the height of the tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,12 +1,14 @@
 #include "binary_trees.h"
 
+size_t binary_tree_height(const binary_tree_t *tree);
+
 /**
  * binary_tree_balance - function that measures the balance factor
  * @tree: pointer to the root node of the tree
  * Return: the balance number
  */
 
-size_t int binary_tree_balance(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
 	size_t left = 0, right = 0;
 
@@ -16,5 +18,27 @@ size_t int binary_tree_balance(const binary_tree_t *tree)
 	left = left + binary_tree_height(tree->left);
 	right = right + binary_tree_height(tree->right);
 
-	return (left - height);
+	return (left - right);
+}
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree
+ * Return: the height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 1, right = 1;
+
+	if (!tree)
+		return (0);
+
+	left = left + binary_tree_height(tree->left);
+	right = right + binary_tree_height(tree->right);
+
+	if (left > right)
+		return (left);
+	else
+		return (right);
 }
